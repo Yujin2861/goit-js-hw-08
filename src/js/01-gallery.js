d42e45d7dd558115d7ea6a6e49e36c3e0d31e3a7
Romaniuk Yevhen <greenapteka.com.ua@gmail.com>
2023-09-10 12:38:27 +0300

    End

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const fotoCardsContainer = document.querySelector('.gallery')
+
+function createFotoCards(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+      </a>
+    </li>
+    `;
+    }).join('');
+    
+}   const fotoCards = (createFotoCards(galleryItems));
+    
+    fotoCardsContainer.insertAdjacentHTML('beforeend', fotoCards);
+
+    const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData:"alt",
+        captionDelay: 250, 
+        captionPosition: "bottom",
+    });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
 
+import _throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const LOCAL_S_KEY = 'feedback-form-state';
+
+let formData = {};
+
+loadForm();
+
+form.addEventListener(`submit`, onFormSubmit);
+form.addEventListener('input', _throttle(onSaveFormInput, 500));
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+    
+    if (email === '' || message === '') {
+        return alert('Заполните все поля')
+    }
+
+    event.currentTarget.reset();
+    console.log(formData);
+    localStorage.removeItem(LOCAL_S_KEY);
+}
+
+function onSaveFormInput(event) {
+    formData = JSON.parse(localStorage.getItem(LOCAL_S_KEY)) || {};
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(LOCAL_S_KEY, JSON.stringify(formData))
+}
+
+function loadForm(){
+    try {
+        let formLoad = JSON.parse(localStorage.getItem(LOCAL_S_KEY))
+        if (!formLoad) {
+            return;
+        }
+
+        formData = formLoad;
+        form.email.value = formData.email || '';
+        form.message.value = formData.message || '';
+
+    } catch (error) {
+        console.error('Error.message ', error.message);
+    }
+}
+
+
